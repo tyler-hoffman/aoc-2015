1c3b31d4c31993654339c2c8478764d8b47aff0f
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2022-08-13 18:01:36 -0400

    Solve 02.b

---
+++ advent_of_code_2015_python/day_02/b.py
@@ -0,0 +1,32 @@
+from advent_of_code_2015_python.day_02.parser import Parser
+from advent_of_code_2015_python.day_02.solver import Box, Solver
+
+
+class Day02PartBSolver(Solver):
+    @property
+    def solution(self) -> int:
+        return sum((self.compute_ribbon(box) for box in self.boxes))
+
+    @staticmethod
+    def compute_ribbon(box: Box) -> int:
+        a, b, c = sorted(box.dimensions)
+        volume = a * b * c
+        smallest_perimiter = 2 * (a + b)
+        return volume + smallest_perimiter
+
+
+def solve(input: str) -> int:
+    parser = Parser()
+    solver = Day02PartBSolver(parser.parse(input))
+
+    return solver.solution
+
+
+def get_solution() -> int:
+    with open("advent_of_code_2015_python/day_02/input.txt", "r") as f:
+        input = f.read()
+    return solve(input)
+
+
+if __name__ == "__main__":
+    print(get_solution())
