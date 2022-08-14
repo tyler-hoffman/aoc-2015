74a3b86c6cc0f1163ac4badce91bd07aa25d2d6e
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2022-08-13 20:45:58 -0400

    Solve 03.a

---
+++ shared/models.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Point:
+    x: int = 0
+    y: int = 0
+
+    def add(self, p: Point) -> Point:
+        return Point(x=self.x + p.x, y=self.y + p.y)
+
+
+LEFT = Point(x=-1)
+RIGHT = Point(x=1)
+UP = Point(y=1)
+DOWN = Point(y=-1)
