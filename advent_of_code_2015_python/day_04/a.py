df8a4a85d6c9080dac2509a695d0cddb05717c19
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2022-08-13 21:35:46 -0400

    Solve 04.a

---
+++ advent_of_code_2015_python/day_04/a.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass, field
+from advent_of_code_2015_python.day_04.parser import Parser
+from advent_of_code_2015_python.day_04.solver import Solver
+
+
+@dataclass
+class Day04PartASolver(Solver):
+    prefix: str = field(init=False, default="00000")
+
+
+def solve(input: str) -> int:
+    parser = Parser()
+    solver = Day04PartASolver(parser.parse(input))
+
+    return solver.solution
+
+
+def get_solution() -> int:
+    with open("advent_of_code_2015_python/day_04/input.txt", "r") as f:
+        input = f.read()
+    return solve(input)
+
+
+if __name__ == "__main__":
+    print(get_solution())
---
+++ advent_of_code_2015_python/day_04/solver.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+import hashlib
+
+
+@dataclass
+class Solver:
+    secret: str
+    prefix: str
+
+    @property
+    def solution(self) -> int:
+        x = 1
+        while True:
+            to_try = f"{self.secret}{x}"
+            hashed = hashlib.md5(to_try.encode("utf-8"))
+            hex = hashed.hexdigest()
+            if hex.startswith(self.prefix):
+                return x
+            x += 1
