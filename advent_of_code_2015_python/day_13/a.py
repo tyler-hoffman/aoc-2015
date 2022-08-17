6653742941142d29ebd0c57c87eb4719202bebf2
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2022-08-17 12:25:15 -0400

    Solve 13.a

---
+++ advent_of_code_2015_python/day_13/a.py
@@ -0,0 +1,47 @@
+import itertools
+from dataclasses import dataclass
+from functools import cached_property
+from typing import Iterable, Mapping, Sequence
+
+from advent_of_code_2015_python.day_13.parser import Parser
+
+
+@dataclass
+class Day13PartASolver:
+    data: Mapping[tuple[str, str], int]
+
+    @property
+    def solution(self) -> int:
+        best_happiness = 0
+        for names in itertools.permutations(self.all_names):
+            best_happiness = max(best_happiness, self.compute_happiness(names))
+        return best_happiness
+
+    def compute_happiness(self, names: tuple[str, ...]) -> int:
+        output = 0
+        for i, name in enumerate(names):
+            output += self.data[(name, names[(i - 1) % len(names)])]
+            output += self.data[(name, names[(i + 1) % len(names)])]
+        return output
+
+    @cached_property
+    def all_names(self) -> frozenset[str]:
+        names = {a for a, b in self.data.keys()}
+        return frozenset(names)
+
+
+def solve(input: str) -> int:
+    data = Parser.parse(input)
+    solver = Day13PartASolver(data)
+
+    return solver.solution
+
+
+def get_solution() -> int:
+    with open("advent_of_code_2015_python/day_13/input.txt", "r") as f:
+        input = f.read()
+    return solve(input)
+
+
+if __name__ == "__main__":
+    print(get_solution())
