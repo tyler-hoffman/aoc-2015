43a0c3ba5f15f7c09dadec8d023ed1ed4889a61d
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2022-08-17 13:03:41 -0400

    Solve 13.b

--- advent_of_code_2015_python/day_13/b.py
+++ advent_of_code_2015_python/day_13/b.py
@@ -1,12 +1,25 @@
+from typing import Mapping
+
 from advent_of_code_2015_python.day_13.parser import Parser
 from advent_of_code_2015_python.day_13.solver import Day13Solver
 
 
 def solve(input: str) -> int:
     data = Parser.parse(input)
+    data = add_me_to_data(data)
+
     solver = Day13Solver(data)
 
     return solver.solution
+
+
+def add_me_to_data(data: Mapping[tuple[str, str], int]):
+    new_data = dict(data)
+    names = Day13Solver(data).all_names
+    for name in names:
+        new_data[("me", name)] = 0
+        new_data[(name, "me")] = 0
+    return new_data
 
 
 def get_solution() -> int:
---
+++ tests/test_day_13/test_b.py
@@ -0,0 +1,5 @@
+from advent_of_code_2015_python.day_13.b import get_solution, solve
+
+
+def test_my_solution():
+    assert get_solution() == 601
