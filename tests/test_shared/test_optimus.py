3d1696ef0b7e6aaa2b6b8ba6119dc3c9bebbfbe3
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2022-09-23 17:24:11 -0400

    Add Optimus::get_primes_up_to

--- tests/test_shared/test_optimus.py
+++ tests/test_shared/test_optimus.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 import pytest
 
 from advent_of_code_2015_python.shared.optimus import Optimus
@@ -19,3 +21,22 @@ from advent_of_code_2015_python.shared.optimus import Optimus
 def test_is_prime(input: int, expected: bool):
     optimus = Optimus()
     assert optimus.is_prime(input) is expected
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (1, []),
+        (2, [2]),
+        (3, [2, 3]),
+        (4, [2, 3]),
+        (5, [2, 3, 5]),
+        (6, [2, 3, 5]),
+        (7, [2, 3, 5, 7]),
+        (8, [2, 3, 5, 7]),
+    ],
+)
+def test_get_primes_up_to(input: int, expected: Iterable[int]):
+    optimus = Optimus()
+    output = optimus.get_primes_up_to(input)
+    assert list(output) == expected
