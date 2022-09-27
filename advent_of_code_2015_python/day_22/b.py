edb642ff25b42a0313f684e5dd4899578bca1664
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2022-09-26 21:22:22 -0400

    Cleanup 22

--- advent_of_code_2015_python/day_22/b.py
+++ advent_of_code_2015_python/day_22/b.py
@@ -1,81 +1,14 @@
-from dataclasses import dataclass
-from queue import PriorityQueue
 from typing import Iterable
+
 from advent_of_code_2015_python.day_22.parser import Parser
-from advent_of_code_2015_python.day_22.shared import (
-    ALL_ABILITIES,
-    Ability,
-    Enemy,
-    GameState,
-    Player,
-)
+from advent_of_code_2015_python.day_22.shared import Day22Solver, GameState
 
 
-@dataclass(frozen=True)
-class Day22PartBSolver:
-    enemy: Enemy
-
-    @property
-    def solution(self) -> int:
-        added_to_queue = set()
-        queue: PriorityQueue[tuple[int, GameState]] = PriorityQueue()
-        queue.put((0, GameState(player=Player(), enemy=self.enemy)))
-
-        while True:
-            _, state = queue.get()
-
-            if state.won:
-                return state.mana_used
-
-            for next_state in self.next_states(state):
-                if not next_state.lost and next_state.key_stats not in added_to_queue:
-                    queue.put((next_state.mana_used, next_state))
-                    added_to_queue.add(next_state.key_stats)
-
-    def next_states(self, old_state: GameState) -> Iterable[GameState]:
-        state = old_state.copy()
-
-        if state.is_player_turn:
-            state.player.hp -= 1
-            if state.player.hp <= 0:
-                yield state
-                return
-
-        self.perform_statuses(state)
-        if state.is_player_turn:
-            usable_abilities = [a for a in ALL_ABILITIES if a.can_do(state)]
-            if not usable_abilities:
-                next_state = state.copy()
-                next_state.player_had_no_move = True
-                next_state.is_player_turn = not next_state.is_player_turn
-                yield next_state
-            for ability in usable_abilities:
-                next_state = state.copy()
-                self.do_player_turn(next_state, ability)
-                next_state.is_player_turn = not next_state.is_player_turn
-                yield next_state
-        else:
-            next_state = state.copy()
-            self.do_enemy_turn(next_state)
-            next_state.is_player_turn = not next_state.is_player_turn
-            yield next_state
-
-    def do_player_turn(self, state: GameState, ability: Ability) -> None:
-        ability.update_state(state)
-        state.player.mp -= ability.cost
-        state.mana_used += ability.cost
-
-    def do_enemy_turn(self, state: GameState) -> None:
-        dmg = max(1, state.enemy.dmg - state.player.armor)
-        state.player.hp -= dmg
-
-    def perform_statuses(self, state: GameState) -> None:
-        for status in state.status_effects:
-            status.on_turn(state)
-            status.remaining -= 1
-            if status.remaining == 0:
-                status.end(state)
-        state.status_effects = {s for s in state.status_effects if s.remaining > 0}
+class Day22PartBSolver(Day22Solver):
+    def get_player_turns(self, state: GameState) -> Iterable[GameState]:
+        state.player.hp -= 1
+        if not state.lost:
+            yield from super().get_player_turns(state)
 
 
 def solve(input: str) -> int:
--- advent_of_code_2015_python/day_22/shared.py
+++ advent_of_code_2015_python/day_22/shared.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
+
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from functools import total_ordering
-from typing import Any, Type
+from queue import PriorityQueue
+from typing import Any, Iterable, Type
 
 
 @dataclass
@@ -37,8 +39,7 @@ class GameState:
     player: Player
     enemy: Enemy
     mana_used: int = 0
-    player_had_no_move: bool = False
-    is_player_turn: bool = True
+    is_player_turn: bool = False
     status_effects: set[StatusEffect] = field(default_factory=set)
 
     def __lt__(self, _) -> bool:
@@ -50,7 +51,6 @@ class GameState:
             enemy=self.enemy.copy(),
             mana_used=self.mana_used,
             is_player_turn=self.is_player_turn,
-            player_had_no_move=self.player_had_no_move,
             status_effects={s.copy() for s in self.status_effects},
         )
 
@@ -60,7 +60,7 @@ class GameState:
 
     @property
     def lost(self) -> bool:
-        return self.player.hp <= 0 or self.player_had_no_move
+        return self.player.hp <= 0
 
     @property
     def key_stats(self) -> tuple[Any, ...]:
@@ -218,3 +218,57 @@ ALL_ABILITIES: set[Ability] = {
     Poison(),
     Recharge(),
 }
+
+
+@dataclass(frozen=True)
+class Day22Solver:
+    enemy: Enemy
+
+    @property
+    def solution(self) -> int:
+        added_to_queue = set()
+        queue: PriorityQueue[tuple[int, GameState]] = PriorityQueue()
+        queue.put((0, GameState(player=Player(), enemy=self.enemy)))
+
+        while True:
+            _, state = queue.get()
+
+            if state.won:
+                return state.mana_used
+
+            for next_state in self.next_states(state):
+                if not next_state.lost and next_state.key_stats not in added_to_queue:
+                    queue.put((next_state.mana_used, next_state))
+                    added_to_queue.add(next_state.key_stats)
+
+    def next_states(self, state: GameState) -> Iterable[GameState]:
+        next_state = state.copy()
+        next_state.is_player_turn = not next_state.is_player_turn
+
+        if next_state.is_player_turn:
+            yield from self.get_player_turns(next_state)
+        else:
+            yield from self.get_enemy_turns(next_state)
+
+    def get_player_turns(self, state: GameState) -> Iterable[GameState]:
+        self.perform_statuses(state)
+        for ability in [a for a in ALL_ABILITIES if a.can_do(state)]:
+            next_state = state.copy()
+            ability.update_state(next_state)
+            next_state.player.mp -= ability.cost
+            next_state.mana_used += ability.cost
+            yield next_state
+
+    def get_enemy_turns(self, state: GameState) -> Iterable[GameState]:
+        self.perform_statuses(state)
+        dmg = max(1, state.enemy.dmg - state.player.armor)
+        state.player.hp -= dmg
+        yield state
+
+    def perform_statuses(self, state: GameState) -> None:
+        for status in state.status_effects:
+            status.on_turn(state)
+            status.remaining -= 1
+            if status.remaining == 0:
+                status.end(state)
+        state.status_effects = {s for s in state.status_effects if s.remaining > 0}
